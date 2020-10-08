3638519e6cbbdedf1d5c0dbef4990e701b540c4c
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-08 16:25:25 +0100

    folder cleaned up

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -10,6 +9,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// For development purpose
+if (module.hot) {
+  module.hot.accept();
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
