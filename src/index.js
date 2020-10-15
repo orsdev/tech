35a3dd49a6c25cdff2593a30e843ac58fb955727
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-15 06:34:27 +0100

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
