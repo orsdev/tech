cd3f861e810036c80ad17b6a3ec2e4c759e27394
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-15 17:57:32 +0100

    navigation for mobile added

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,59 @@
-import React from 'react';
-import { Menu } from 'antd';
+import React, { useState } from 'react';
+import { Anchor, Drawer, Button } from 'antd';
+
+const { Link } = Anchor;
 
 function Nav() {
+
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <>
-      <nav className="nav">
+      <nav className="nav" id="home">
         <a href="#home" className="logo">
           <i className="fa fa-bolt" aria-hidden="true"></i>
           <span>TECH</span>
         </a>
-        <Menu mode="horizontal" defaultSelectedKeys={['home']}>
-          <Menu.Item key="home">Home</Menu.Item>
-          <Menu.Item key="about">About</Menu.Item>
-          <Menu.Item key="feature">Features</Menu.Item>
-          <Menu.Item key="hiw">How it works</Menu.Item>
-          <Menu.Item key="faq">FAQ</Menu.Item>
-          <Menu.Item key="pricing">Pricing</Menu.Item>
-          <Menu.Item key="contact">Contact</Menu.Item>
-        </Menu>
+        <div className="desktop-nav">
+          <Anchor affix={false} defaultSelectedKeys={['home']} targetOffset={64}>
+            <Link href="#home" title="Home" />
+            <Link href="#about" title="About" />
+            <Link href="#features" title="Features" />
+            <Link href="#hiw" title="How it works" />
+            <Link href="#faq" title="FAQ" />
+            <Link href="#pricing" title="Pricing" />
+            <Link href="#contact" title="Contact" />
+          </Anchor>
+        </div>
+        <div className="mobile-nav">
+          <Button type="primary" onClick={showDrawer}>
+            <i className="fa fa-bars" aria-hidden="true"></i>
+          </Button>
+          <Drawer
+            placement="right"
+            closable={false}
+            onClose={onClose}
+            visible={visible}
+          >
+            <Anchor affix={false} defaultSelectedKeys={['home']} targetOffset={64}>
+              <Link href="#home" title="Home" />
+              <Link href="#about" title="About" />
+              <Link href="#features" title="Features" />
+              <Link href="#hiw" title="How it works" />
+              <Link href="#faq" title="FAQ" />
+              <Link href="#pricing" title="Pricing" />
+              <Link href="#contact" title="Contact" />
+            </Anchor>
+          </Drawer>
+        </div>
       </nav>
     </>
   )
