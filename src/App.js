7807eb680e54e046ae6c5f723988bf4668569764
Ogundare Samuel Olamide <oluwadareysamuel@yahoo.com>
2020-10-15 06:35:10 +0100

    nav & hero component used in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Layout } from 'antd';
+import Nav from './components/nav';
+import 'antd/dist/antd.css';
+import './custom.css';
+import Hero from './components/hero';
+
+const { Header } = Layout;
 
 const App = () => {
 
   return (
     <>
-      <h1>HOME</h1>
+      <Header>
+        <Nav />
+      </Header>
+      <Hero />
     </>
   );
 }
